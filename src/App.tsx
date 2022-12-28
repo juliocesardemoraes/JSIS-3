22cb21353e055bf625c0333e2214602041579522
julio cesar <jcmcf2012@gmail.com>
2022-12-28 19:07:53 -0300

    feat: text editor with emmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import Editor from "@monaco-editor/react";
+import { emmetHTML, emmetCSS } from "emmet-monaco-es";
+import { useState } from "react";
+
 export default function App() {
+  const [isEditorReady, setIsEditorReady] = useState(false);
+
+  const handleEditorDidMount = () => {
+    emmetHTML(window.monaco);
+    emmetCSS(window.monaco);
+    setIsEditorReady(true);
+  };
+
   return (
     <>
       <h1>Teste</h1>
+      <Editor
+        className="editor-main-container"
+        theme={"dark"}
+        language={"html"}
+        height="90vh"
+        beforeMount={handleEditorDidMount}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+      />
     </>
   );
 }
